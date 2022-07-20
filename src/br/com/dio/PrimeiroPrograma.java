726e7461c9c2726fefcd37b6f4b0450e120960c8
metal <git@github.com:RAYSAFIGUEIREDO/Dio-Java-Intellij.git>
2022-07-20 18:20:17 -0300

    criando class livro

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -3,11 +3,17 @@ package br.com.dio;
 import br.com.dio.model.Gato;
 
 public class PrimeiroPrograma {
+
+
     public static void main(String[] args) {
 
         Gato gato = new Gato();
 
         System.out.println(gato);
+
+
+       /* Livro livro1 = new Livro(nome: "O problema dos 3 corpos", numPaginas:300);
+System.out.println(livro1);*\
 
 
        /* int a = 5;
@@ -16,3 +22,39 @@ public class PrimeiroPrograma {
     }
 
 }
+class Livro{
+
+  private String nome;
+  private Integer numPaginas;
+
+
+    public Livro(String nome, Integer nunPaginas) {
+        this.nome = nome;
+        this.numPaginas = numPaginas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNunPaginas() {
+        return numPaginas;
+    }
+
+    public void setNunPaginas(Integer nunPaginas) {
+        this.numPaginas = nunPaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPaginas=" + numPaginas +
+                '}';
+    }
+}
+
--- src/br/com/dio/model/Gato.java
+++ src/br/com/dio/model/Gato.java
@@ -63,5 +63,7 @@ public class Gato {
                 ", cor='" + cor + '\'' +
                 ", idade=" + idade +
                 '}';
+
     }
+
 }
